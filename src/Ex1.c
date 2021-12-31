ee921e23aa874a7c620174e8567cb9b1b66c14bd
QuestionMark001 <604661720@qq.com>
2021-12-31 11:25:02 +0800

    update Ex1

--- src/Ex1.c
+++ src/Ex1.c
@@ -1,12 +1,26 @@
+/* 九九乘法表 */
 #include <stdio.h>
+//#include <stdlib.h>   //Windows操作系统下请务必将这一行注释去掉！！！
 
 int main(void)
 {
+    printf("九九乘法表\n");
+    printf("按下Enter键开始运行\n");
+    getchar();
+
     int x,y;
 
-    for (x = 1; i <= ; i++)
+    for (x = 1; x <= 9; x++)
     {
-        /* code */
+        for (y = 1; y <= x; y++)
+        {
+            printf("%d*%d=%-3d",y,x,x*y);
+        }
+        printf("\n");
+        
     }
     
+    //system("pause");   Windows操作系统下请务必将这一行注释去掉！！！
+    return 0;
+
 }
